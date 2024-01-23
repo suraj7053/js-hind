3fadd8a385c521aaab22efbc1db1dc9c996c4c27
suraj7053 <157237690+suraj7053@users.noreply.github.com>
2024-01-23 05:30:36 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144322
+let accountEmail = "suraj1966@gmail.com"
+var accountPassword = "12345"
+accountCity = "Patna"
+
+// accountId = 2 constant vlue donot changes
+accountEmail = "dddhdjeh@google.com"
+accountPassword = "2345654"
+accountCity = "Delhi"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
