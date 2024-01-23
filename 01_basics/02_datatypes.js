78912c20419b22963e58bd4b7565b397ddf3c551
suraj7053 <157237690+suraj7053@users.noreply.github.com>
2024-01-23 05:53:41 +0000

    commit

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all code as newer version
+
+// alert(3+3) //we are using nodejs, not browser 
+console.log(3+3)
+console.log("suraj") //learn documentation from mdn from mozilla and ecma
+
+let name = "Suraj"
+let age = 18
+let isloggedIn = false;
+let state;
+
+//number => 3 to power 53
+//bigint
+//string => ""
+// boolean => true/false 
+//null => standalolne value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof null); //null object datatype
+
