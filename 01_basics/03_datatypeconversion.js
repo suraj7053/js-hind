9d9fb56b2c3e284f25086bc7f3a4f7e1e44f5b62
suraj7053 <157237690+suraj7053@users.noreply.github.com>
2024-01-23 06:22:38 +0000

    commit

---
+++ 01_basics/03_datatypeconversion.js
@@ -0,0 +1,27 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+/*
+"33" =>33
+"22abc" => NaN
+true => 1; false =>0
+
+*/
+let isloggedIn = 1
+let booleanIsLoggedIn = Boolean(isloggedIn)
+console.log(booleanIsLoggedIn);
+//1 =>true; 0 =>false
+//"" => false
+//"hitesh" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)//capital s in string
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
